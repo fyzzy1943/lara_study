a5ba95d4d4d27de5965202386c710e0cca3f9663
silence <csiris@126.com>
2015-09-19 18:27:23 +0800

    .

---
+++ resources/views/articles/lists.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Article lists page</title>
+    </head>
+<body>
+<h1>Hello Laravel</h1>
+</body>
+</html>
