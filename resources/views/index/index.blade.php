3bc9ca7b1b87b2493e5a550475d721e1f31418f2
silence <Administrator@pc-PC>
2015-10-05 23:32:15 +0800

    .

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -4,13 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <title>Rainy mood</title>
 
-    <!-- Bootstrap -->
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -19,7 +16,7 @@
     <style>
         body {
             background: url(/img/index.jpg) no-repeat;
-            background-size: 100% auto;
+            background-size: 100% auto;        	
         }
 
         #title {
@@ -36,10 +33,6 @@
 </head>
 <body>
 
-{{--<audio autoplay="ture" loop="true">--}}
-{{--<source src="/music/RainyMood.mp3" />--}}
-{{--</audio>--}}
-
     <div id="jquery_jplayer_1"></div>
 
     <div id="container">
@@ -47,27 +40,32 @@
 
         <div><img id="change_volume" src="/img/volume/vol3.png" /></div>
     </div>
+    <div style="display: none">
+        <img src="/img/volume/vol2.png" />
+        <img src="/img/volume/vol1.png" />
+        <img src="/img/volume/vol0.png" />
+    </div>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <!-- jplayer -->
 <script src="/js/jquery.jplayer.min.js"></script>
 <script>
     $(document).ready(function () {
+    
         var vol=3;
         var jp = $('#jquery_jplayer_1');
 
         jp.jPlayer({
             ready: function () {
                 $(this).jPlayer('setMedia', {
-                    mp3: '/music/RainyMood.mp3'
+                    m4a: '/music/0.m4a',
+                    oga: '/music/0.ogg'
                 }).jPlayer('play');
             },
             swfPath: 'swf',
-            supplied: 'mp3',
-            loop: 'true',
+            supplied: 'm4a, oga',
+            loop: true,
             volume: 1.0
         });
 
@@ -94,6 +92,8 @@
                     break;
             }
         });
+
+        
     });
 </script>
 
